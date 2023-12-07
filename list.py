f1a078b127c16d7f87603bb92fe1556c0fddfff1
r-alx <kycoin890@gmail.com>
2023-12-07 21:00:42 +0700

    list methods append ,clear,copy,count

---
+++ list.py
@@ -0,0 +1,10 @@
+card = ["One Piece","Skibidi"]
+#append method to add item in list
+card.append("Naruto")
+## clear method to clear the list
+#card.clear()
+#copy method copy the list 
+copy = card.copy()
+print(copy.count("Naruto"))
+
+print(card, copy)
