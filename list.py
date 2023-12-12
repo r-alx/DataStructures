b02537d9d318fbba6399a7efe73743ce3ee2b21a
r-alx <kycoin890@gmail.com>
2023-12-12 21:02:23 +0700

    Complete list methods

--- list.py
+++ list.py
@@ -1,23 +1,26 @@
+"""Ïmplementation of the list methods"""
+
+# our list sample
 card = ["One Piece","Skibidi","Mini"]
 
+#Append:Put item in the list
 card.append("Naruto")
-card.append("Mini")
-
+#Copy: make a copy of the list.
 copy = card.copy()
-print(copy.count("Naruto"))
-
-print(card)
-
-print(dir(card))
-
+#Extend: put the items to the original list by unpacking the collection.
 card.extend(["no","yes","???"])
-
-
-print(card)
-
-print("-------------------Älex presentation below--------------------------")
-print(card)
+#Index:Finds the index of the specified item.
 print(card.index("no"))
-
+#Insert: Put the items on the specified index.
 card.insert(5,"temp")
-print(card)
+#Pop:remove the item on the specified index.
+card.pop(2)
+#Remove:remove the item from the list
+card.remove("temp")
+#Reverse:reverse the item from the list
+card.reverse()
+#Sort:sort the item in a descending or ascending  
+n_list = [5,2,6,3,4,1,7,0,9,8]
+n_list.sort(reverse=True)
+print(n_list)
+# help(n_list.sort)
---
+++ presatation.py
@@ -0,0 +1,4 @@
+sword = ["wodden sword","stone sword","iron sword","gold sword","diamond sword","netherite sword","sharpness10"]
+sword.insert(6,"Efichency4")
+print(sword)
+
