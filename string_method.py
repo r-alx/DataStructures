ae58f4ef157cf9f131dc1ea61a1d97f9746f0c8b
r-alx <kycoin890@gmail.com>
2024-01-07 18:00:29 +0700

    string_method.py

--- string_method.py
+++ string_method.py
@@ -1,6 +1,19 @@
-text  = "Ac OpycaT"
+text ="WHO DID THIS"
 print(dir(text))
-print("___________________________________Seprator_______________________________________")
-print(text.istitle())
+a = "I dunno"
+b = "tell truth or I use the lie detector"
 
+# collect = ["apple","banana","orange"]
+# print("@".ljust(collect))
 
+# beg = "starter"
+# just = beg.rjust(100)
+# print(just,"learning the ljust")
+
+# print(text.lower())
+
+text_2 = "@#      text to strip           @@@##@@    "
+# print(text_2.rstrip("@# "))
+# print(text_2.replace("@","%"))
+text_3 = "skip to go ahead"
+print(text_3.rindex("go"))
