b22075607fe325785016dd1b6a2721d681d7add0
r-alx <kycoin890@gmail.com>
2024-01-23 17:59:08 +0700

    string method pro4.py

---
+++ program4_string.py
@@ -0,0 +1,52 @@
+# # take an input from the user and check its remainder from 3.
+# num = int(input("Enter the num:"))
+# remainder = num % 3
+# print(remainder)
+
+# # write a program to get a number from the user and prints its last digit.
+# ps = int(input("Enter the num:"))
+# last_digit = ps % 10
+# print("The last digit is:",last_digit)
+
+# # write a program to take a number from the user and print whether it is even or odd.
+# sos = int(input("Enter the hunderd num:"))
+# if sos % 2 == 0:
+#     print("the num is even")
+# else:
+#     print("The num is odd")
+
+
+# write a program to get two numbers and check whether the given number is smaller than other or not.
+# ko = int(input("Enter the first num:"))
+# ok = int(input("Enter the sec num:"))
+# if ko > ok:
+#     print("Ko is bigger")
+# elif ok > ko:
+#     print("ok is bigger")
+# else:
+#     print("both are equal")
+
+
+#write a program to get a number from user and print positve if its greater than 0.
+dna = int(input("Enter the num"))
+if dna > 0:
+    print("dna win the first prize!!!!")
+
+# write a program to get a number from the user and print postive if it greater than zero, else print negative.
+bin = int(input("Enter the num"))
+if bin > 0:
+    print("Bin has the first prize!!!")
+else:
+    print("0 win the first prize")    
+
+#write a program to get a number form the user and print positve if it greater than zero, negative if its below than zero and print zero if its zero.
+biome =int(input("Enter the num:"))
+if biome > 0:
+    print("biome wins!!!")
+elif biome < 0:
+    print("0 wins!!!")
+else:
+    print("same...")
+
+
+ 
